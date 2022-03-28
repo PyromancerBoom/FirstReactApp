821e1c264e262e185c4b46c97e76a7e5870300f2
PyromancerBoom <priyanshmagic@gmail.com>
2022-03-28 23:46:52 +0530

    Basic code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  // Calling the useState func to set default state of app when it starts
+  // This function returns an array
+  // We are basically destructuring the array
+  const [todos, setTodos] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TodoList />
+    <input type="text" />
+    <button>Add todo</button>
+    <button>Clear</button>
+    <div>0 left to do</div>
+    
+    </>
+  )
 }
 
 export default App;
