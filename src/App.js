2beb94f2a8372102fe77f1c75046504497cd7ce5
PyromancerBoom <priyanshmagic@gmail.com>
2022-03-31 18:37:11 +0530

    some code but it ain't working :/

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   // Calling the useState func to set default state of app when it starts
   // This function returns an array
   // We are basically destructuring the array
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([{id: 1, name: 'Todo 1', complete: false}])
   return (
+    // Passing the useState here
+    // We have a prop todos and we pass it
     <>
-    <TodoList />
+    <TodoList todos={todos}/>
     <input type="text" />
     <button>Add todo</button>
     <button>Clear</button>
